ea103409c377cdd7f6c7c675b955f280396ab193
Harisankar <harisankar@uniqnex360.com>
2026-05-15 16:46:17 +0530

    feat:Added changes in defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', 
+  publicDir: 'public',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
   },
 });
